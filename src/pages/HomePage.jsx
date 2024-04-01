6fc0006aa5af73988f3f24c58325b157e8321483
venkatesh-sdev <venkatesh.dev.main@gmail.com>
2024-04-01 21:13:02 +0530

    dumy

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const HomePage = () => {
+    const navigate = useNavigate();
     return (
-        <div>HomePage</div>
+        <div>
+            <button onClick={() => navigate('/signup')}>Go SignUp</button>
+        </div>
     )
 }
 
